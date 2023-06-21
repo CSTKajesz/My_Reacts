6373b332a03d123b08a3ce6754b6cbf9fe1e2cd8
CSTKajesz <tothkajesz@gmail.hu>
2023-06-21 08:28:40 +0200

    text_opacity

---
+++ TailwindCSS/first/src/challenges/alaolasz/text_opacity.js
@@ -0,0 +1,104 @@
+<ul class="container mx-auto leading-none divide-y divide-gray-400 divide-dotted" style="font-family: Raleway">
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-0 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-0</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-5 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-5</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-10 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-10</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-20 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-20</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-25 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-25</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-30 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-30</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-40 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-40</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-50 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-50</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-60 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-60</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-70 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-70</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-75 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-75</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-70 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-70</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-75 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-75</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-80 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-80</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-90 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-90</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-95 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-95</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div>
+      <div class=" text-opacity-100 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-100</div>
+  </li>
+</ul>
