87c8fa96bf7a9e2f83237e0057530f0b220986e3
CSTKajesz <tothkajesz@gmail.hu>
2023-06-21 08:27:54 +0200

    text_alignment

---
+++ TailwindCSS/first/src/challenges/alaolasz/text_alignment.js
@@ -0,0 +1,34 @@
+<ul class="container mx-auto divide-y divide-gray-400 divide-dotted" style="font-family: Raleway">
+  <li class="flex items-center justify-between px-4 py-2">
+    <div class="text-left">
+      <div class="font-light text-5xl align-middle mb-2">Rex</div>
+        <p class="text-md leading-tight">Rex is the Mayor of Binaryville and a well-loved personality in town. He rose to robotdom from a microprocessor plant on the south side of town, where many famous and influential robots before him were conceived.</p>
+      <div class="text-xs font-mono font-light leading-tight text-gray-500 mt-2"><span class="font-bold">text-align</span>: left</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">text-left</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div class="text-center">
+      <div class="font-light text-5xl align-middle mb-2">Dolores</div>
+      <p class="text-md leading-tight">Dolores robot is a computational genius who performs calculations for various Binaryville businesses and organizations. In her spare time, Dolores has been noodling on the creation of the perfect web browser that will make all other browsers obsolete. We're rooting for you, Dolores!</p>
+      <div class="text-xs font-mono font-light leading-tight text-gray-500 mt-2"><span class="font-bold">text-align</span>: center</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">text-center</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div class="text-right">
+      <div class="font-light text-5xl align-middle mb-2">Bubbles</div>
+      <p class="text-md leading-tight">Bubble Gum robot, or “Bubbles” for short, is from a family of track-footed robots that ¬originated from an experiment melding candy vending machines with robotics in the early 1980s. Bubbles is a favorite of Binaryville, not just because she generously dispenses candy, but also because she has one of the more "bubbly" personalities of the villagers.</p>  
+      <div class="text-xs font-mono font-light leading-tight text-gray-500 mt-2"><span class="font-bold">text-align</span>: right</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">text-right</div>
+  </li>
+  <li class="flex items-center justify-between px-4 py-2">
+    <div class="text-justify">
+      <div class="font-light text-5xl align-middle mb-2">Fred</div>
+      <p class="text-md leading-tight">Fred is inquisitive and creative, and always conjuring up ways to improve Binaryville and the lives of its inhabitants. He's been at the forefront of many inventions, but has a special affinity for improving the world through alternative energy and green-living. Fred's engineering collaborations with the City of Binaryville have helped the city reduce its energy consumption year over year, and he's twice been awarded the highly coveted Binaryville Medallion of Honor.</p>  
+      <div class="text-xs font-mono font-light leading-tight text-gray-500 mt-2"><span class="font-bold">text-align</span>: justify</div>
+    </div>
+    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">text-justify</div>
+  </li>
+</ul>
