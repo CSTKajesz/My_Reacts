4c41bbcf2a272d68f6e51d01acc7da56896f642d
CSTKajesz <tothkajesz@gmail.com>
2023-07-01 16:36:30 +0200

    Text opacity and color done

--- TailwindCSS/first/src/pages/typography/TextDirectives.js
+++ TailwindCSS/first/src/pages/typography/TextDirectives.js
@@ -3,7 +3,237 @@ const TextDirectives = () => {
     return (
         <section id="textdirectives">
             <div className="text-2xl border-t-8 border-b-2 mb-8">Text Colors</div>
-            <ul></ul>
+            <table className="border-separate [border-spacing:0.75rem]">
+
+                <tr className="row">
+                    <th className="font-bold pr-4">Text colors</th>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-black">Black</th>
+                    <td className="text-black-50">50</td>
+                    <td className="text-black-100">100</td>
+                    <td className="text-black-200">200</td>
+                    <td className="text-black-300">300</td>
+                    <td className="text-black-400">400</td>
+                    <td className="text-black-500">500</td>
+                    <td className="text-black-600">600</td>
+                    <td className="text-black-700">700</td>
+                    <td className="text-black-800">800</td>
+                    <td className="text-black-900">900</td>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-black">Gray</th>
+                    <td className="text-gray-50">50</td>
+                    <td className="text-gray-100">100</td>
+                    <td className="text-gray-200">200</td>
+                    <td className="text-gray-300">300</td>
+                    <td className="text-gray-400">400</td>
+                    <td className="text-gray-500">500</td>
+                    <td className="text-gray-600">600</td>
+                    <td className="text-gray-700">700</td>
+                    <td className="text-gray-800">800</td>
+                    <td className="text-gray-900">900</td>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-black">Red</th>
+                    <td className="text-red-50">50</td>
+                    <td className="text-red-100">100</td>
+                    <td className="text-red-200">200</td>
+                    <td className="text-red-300">300</td>
+                    <td className="text-red-400">400</td>
+                    <td className="text-red-500">500</td>
+                    <td className="text-red-600">600</td>
+                    <td className="text-red-700">700</td>
+                    <td className="text-red-800">800</td>
+                    <td className="text-red-900">900</td>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-black">Yellow</th>
+                    <td className="text-yellow-50">50</td>
+                    <td className="text-yellow-100">100</td>
+                    <td className="text-yellow-200">200</td>
+                    <td className="text-yellow-300">300</td>
+                    <td className="text-yellow-400">400</td>
+                    <td className="text-yellow-500">500</td>
+                    <td className="text-yellow-600">600</td>
+                    <td className="text-yellow-700">700</td>
+                    <td className="text-yellow-800">800</td>
+                    <td className="text-yellow-900">900</td>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-black">Green</th>
+                    <td className="text-green-50">50</td>
+                    <td className="text-green-100">100</td>
+                    <td className="text-green-200">200</td>
+                    <td className="text-green-300">300</td>
+                    <td className="text-green-400">400</td>
+                    <td className="text-green-500">500</td>
+                    <td className="text-green-600">600</td>
+                    <td className="text-green-700">700</td>
+                    <td className="text-green-800">800</td>
+                    <td className="text-green-900">900</td>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-black">Blue</th>
+                    <td className="text-blue-50">50</td>
+                    <td className="text-blue-100">100</td>
+                    <td className="text-blue-200">200</td>
+                    <td className="text-blue-300">300</td>
+                    <td className="text-blue-400">400</td>
+                    <td className="text-blue-500">500</td>
+                    <td className="text-blue-600">600</td>
+                    <td className="text-blue-700">700</td>
+                    <td className="text-blue-800">800</td>
+                    <td className="text-blue-900">900</td>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-black">Indigo</th>
+                    <td className="text-indigo-50">50</td>
+                    <td className="text-indigo-100">100</td>
+                    <td className="text-indigo-200">200</td>
+                    <td className="text-indigo-300">300</td>
+                    <td className="text-indigo-400">400</td>
+                    <td className="text-indigo-500">500</td>
+                    <td className="text-indigo-600">600</td>
+                    <td className="text-indigo-700">700</td>
+                    <td className="text-indigo-800">800</td>
+                    <td className="text-indigo-900">900</td>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-black">Purple</th>
+                    <td className="text-purple-50">50</td>
+                    <td className="text-purple-100">100</td>
+                    <td className="text-purple-200">200</td>
+                    <td className="text-purple-300">300</td>
+                    <td className="text-purple-400">400</td>
+                    <td className="text-purple-500">500</td>
+                    <td className="text-purple-600">600</td>
+                    <td className="text-purple-700">700</td>
+                    <td className="text-purple-800">800</td>
+                    <td className="text-purple-900">900</td>
+                </tr>
+                <tr className="row">
+                    <th className="font-bold text-pink-500">Pink</th>
+                    <td className="text-pink-50">50</td>
+                    <td className="text-pink-100">100</td>
+                    <td className="text-pink-200">200</td>
+                    <td className="text-pink-300">300</td>
+                    <td className="text-pink-400">400</td>
+                    <td className="text-pink-500">500</td>
+                    <td className="text-pink-600">600</td>
+                    <td className="text-pink-700">700</td>
+                    <td className="text-pink-800">800</td>
+                    <td className="text-pink-900">900</td>
+                </tr>
+
+            </table>
+
+
+            <div className="text-2xl border-t-8 border-b-2 mt-20 mb-8">Text Opacity</div>
+            <ul class="container mx-auto leading-none divide-y divide-gray-400 divide-dotted">
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-0 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-0</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-5 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-5</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-10 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-10</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-20 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-20</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-25 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-25</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-30 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-30</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-40 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-40</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-50 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-50</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-60 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-60</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-70 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-70</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-75 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-75</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-70 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-70</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-75 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-75</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-80 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-80</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-90 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-90</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-95 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-95</div>
+                </li>
+                <li class="flex items-center justify-between px-4 py-2">
+                    <div>
+                        <div class=" text-opacity-100 bg-indigo-500 text-white rounded-2 px-2 py-1 rounded inline-block font-light text-2xl align-middle mb-2">Dolores</div>
+                    </div>
+                    <div class="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 text-white bg-pink-500 rounded">text-opacity-100</div>
+                </li>
+            </ul>
 
 
         </section>
