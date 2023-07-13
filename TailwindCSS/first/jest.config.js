e1296699d1b1b5742c952ccd807a04dbfa559e2d
CSTKajesz <tothkajesz@gmail.com>
2023-07-13 22:02:17 +0200

    deleting the wrong test

--- TailwindCSS/first/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testMatch: ['**/__tests__/**/*.test.js'],
-    testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js'],
-    // esm: 'babel',
-};
