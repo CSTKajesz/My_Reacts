9d4fb8b9d7654844f9d804d77604ab97f9d4d53d
CSTKajesz <tothkajesz@gmail.com>
2023-08-06 13:05:21 +0200

    With CLI page done

--- TailwindCSS/first/src/pages/home/InstallCLI.js
+++ TailwindCSS/first/src/pages/home/InstallCLI.js
@@ -13,14 +13,32 @@ const InstallCLI = () => {
                 </p>
                 <ul className="container mx-auto divide-y divide-gray-400 divide-dotted">
                     <li className="flex items-center justify-between px-4 py-2">
-                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">1</div>
-                        npm install -D tailwindcss
-                        <br />
-                        npx tailwindcss init
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 mr-5 rounded text-white bg-pink-500 rounded-2">1</div>
+
                         <div className="select-auto text-md">Install tailwindcss via npm, and create your tailwind.config.js file.</div>
                         <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">Install Tailwind CSS</div>
-                        <div className='relative z-10 -ml-10 col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10'></div>
                     </li>
+                    <li className="flex items-center justify-between px-4 py-2">
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 mr-5 rounded text-white bg-pink-500 rounded-2">2</div>
+                        <div className="select-auto text-md">Add the paths to all of your template files in your tailwind.config.js file.</div>
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">Configure your template paths</div>
+                    </li>
+                    <li className="flex items-center justify-between px-4 py-2">
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 mr-5 rounded text-white bg-pink-500 rounded-2">3</div>
+                        <div className="select-auto text-md">Add the @tailwind directives for each of Tailwind’s layers to your main CSS file.</div>
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">Add the Tailwind directives to your CSS</div>
+                    </li>
+                    <li className="flex items-center justify-between px-4 py-2">
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 mr-5 rounded text-white bg-pink-500 rounded-2">4</div>
+                        <div className="select-auto text-md">Run the CLI tool to scan your template files for classes and build your CSS.</div>
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">Start the Tailwind CLI build process</div>
+                    </li>
+                    <li className="flex items-center justify-between px-4 py-2">
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 mr-5 rounded text-white bg-pink-500 rounded-2">5</div>
+                        <div className="select-auto text-md">Add your compiled CSS file to the head and start using <br />Tailwind’s utility classes to style your content.</div>
+                        <div className="text-xs font-semibold font-mono whitespace-nowrap px-2 py-1 ml-5 rounded text-white bg-pink-500 rounded-2">Start using Tailwind in your HTML</div>
+                    </li>
+
                 </ul>
 
                 <Link to='https://tailwindcss.com/docs/installation'>
