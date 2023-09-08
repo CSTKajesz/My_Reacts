06f60416ac5d0709bb384ac057adce5e18cdb578
CSTKajesz <tothkajesz@gmail.com>
2023-09-08 15:01:06 +0200

    Blog section

--- Responsive-With-Figma/front-end/src/components/Blog/Blog.jsx
+++ Responsive-With-Figma/front-end/src/components/Blog/Blog.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import "./Blog.css"
+import React, { useEffect, useState } from "react";
+import "./Blog.css";
+import { blog1, blog2, blog3 } from '../../assets'
+import posts from "./posts"
 
 const Blog = () => {
-    return (
-        <div>Blog</div>
-    )
-}
 
-export default Blog
+    return (
+        <section id="blog" className="pink">
+            <div className="wrapper">
+                <h2 className="light">Latest Posts</h2>
+                <div className="content-container">
+                    {/* Ahhoz, hogy elkerüljük a kódduplikálást, és minden egyes postot lemásoljuk és átírjuk használjunk map-et*/}
+                    {/* Dinamikusan létrehozott posztok a 'posts' tömb alapján */}
+                    {posts.map((post, index) => (
+                        <div className="post" key={index}>
+                            <div className="tag">{post.tag}</div>
+                            <a href="#">
+                                <img src={post.image} alt="" />
+                            </a>
+                            <a href="#">
+                                <h3 className="post-title">{post.title}</h3>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="btn-container">
+                    <a href="#" className="all-posts-btn">
+                        All Posts
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+};
+export default Blog;
---
+++ Responsive-With-Figma/front-end/src/components/Blog/posts.js
@@ -0,0 +1,21 @@
+import { blog1, blog2, blog3 } from '../../assets'
+
+const posts = [
+    {
+        tag: "DAW",
+        image: blog1,
+        title: "How To Use Drum Machine in Logic Pro X",
+    },
+    {
+        tag: "Mixing",
+        image: blog2,
+        title: "How To Mix Guitars Effectively",
+    },
+    {
+        tag: "Vox",
+        image: blog3,
+        title: "The Real Power of Harmonies in Music Production",
+    },
+];
+
+export default posts;
