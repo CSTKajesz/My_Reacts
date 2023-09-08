4b4c3970e52ba410c7881c84c20c386752f8cc5d
CSTKajesz <tothkajesz@gmail.com>
2023-09-08 12:27:24 +0200

    Header and Navbar

--- Responsive-With-Figma/front-end/src/components/Header/Header.jsx
+++ Responsive-With-Figma/front-end/src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import "./Header.css"
+import Navbar from '../Navbar/Navbar'
 
 
 const Header = () => {
     return (
         <header>
             <div className='wrapper'>
-                <p className='course-name'>Sound Design Masterclass</p>
+                <Navbar />
+                <div className='cta'>
+                    <p className='course-name'>Sound Design Masterclass</p>
+                    <h1>Learn the Art of Sound Design</h1>
+                    <a href="#" className='demo-btn'>Demo Lesson</a>
+                </div>
             </div>
         </header>
     )
--- Responsive-With-Figma/front-end/src/components/Navbar/Navbar.jsx
+++ Responsive-With-Figma/front-end/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,35 @@ import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <>
+            <nav>
+                <a href="#" className="logo">
+                    Sound<span className="red">DZign</span>
+                </a>
+
+                <ul>
+                    <li>
+                        <a href="#topics">Course Details</a>
+                    </li>
+
+                    <li>
+                        <a href="#info">About</a>
+                    </li>
+
+                    <li>
+                        <a href="#blog">Blog</a>
+                    </li>
+
+                    <li>
+                        <a href="#testimonials">Testimonials</a>
+                    </li>
+                </ul>
+
+                {/* <div onClick={() => setIsActive(true)} className="menu-icon">
+                    <img src={menuIcon} alt="" />
+                </div> */}
+            </nav>
+        </>
     )
 }
 
