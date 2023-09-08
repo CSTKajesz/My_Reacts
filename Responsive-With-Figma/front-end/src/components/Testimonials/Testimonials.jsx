061d9d863dd38c1a14f5c3d73a5408699b871b62
CSTKajesz <tothkajesz@gmail.com>
2023-09-08 15:19:41 +0200

    Section Testimonials

--- Responsive-With-Figma/front-end/src/components/Testimonials/Testimonials.jsx
+++ Responsive-With-Figma/front-end/src/components/Testimonials/Testimonials.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import "./Testimonials.css"
+import testimonials from "./testimonals"
 
 
 const Testimonials = () => {
     return (
-        <div>Testimonials</div>
+        <section id="testimonials" className="dark-gray">
+            <div className="wrapper">
+                <h2>What our students say?</h2>
+
+                <div className="content-container">
+                    {testimonials.map((testimonial, index) => (
+                        <div className="testimonial" key={index}>
+                            <a href="#">
+                                <img src={testimonial.image} alt="" />
+                            </a>
+                            <div className="reviewer-details">
+                                <div className="name">{testimonial.name}</div>
+                                <div className="company-name">{testimonial.company}</div>
+                                <div className="review">{testimonial.message}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     )
 }
 
---
+++ Responsive-With-Figma/front-end/src/components/Testimonials/testimonals.js
@@ -0,0 +1,24 @@
+import { testimonial1, testimonial2, testimonial3 } from '../../assets'
+
+const testimonals = [
+    {
+        name: "Peter AdamsDAW",
+        image: testimonial1,
+        message: "This is a great course. I got to learn a lot.",
+        company: "Google"
+    },
+    {
+        name: "Robert Fox",
+        image: testimonial2,
+        message: "I got to learn a lot about Music Production with this course. Thanks :)",
+        company: "Instagram"
+    },
+    {
+        name: "Emily Smith",
+        image: testimonial3,
+        message: "Awesome! Great job!",
+        company: "Apple"
+    },
+];
+
+export default testimonals;
