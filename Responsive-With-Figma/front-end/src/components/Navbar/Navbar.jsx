b00871c357292b15e8e0b3102c8c010dc0cb58db
CSTKajesz <tothkajesz@gmail.com>
2023-09-09 08:38:21 +0200

    Added responsive design to Nav

--- Responsive-With-Figma/front-end/src/components/Navbar/Navbar.jsx
+++ Responsive-With-Figma/front-end/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import "./Navbar.css"
+import React, { useState } from "react";
+import "./Navbar.css";
+import { menuIcon, closeIcon } from "../../assets";
+
 
 const Navbar = () => {
+    const [isActive, setIsActive] = useState(false);
+
     return (
         <>
             <nav>
@@ -27,12 +31,44 @@ const Navbar = () => {
                     </li>
                 </ul>
 
-                {/* <div onClick={() => setIsActive(true)} className="menu-icon">
+                <div onClick={() => setIsActive(true)} className="menu-icon">
                     <img src={menuIcon} alt="" />
-                </div> */}
+                </div>
             </nav>
-        </>
-    )
-}
+            <div className={`mobile-menu-container ${isActive ? "active" : ""}`}>
+                <div onClick={() => setIsActive(false)} className="close-icon">
+                    <img src={closeIcon} alt="" />
+                </div>
 
-export default Navbar
+                <ul className="menu-items">
+                    <li>
+                        {/* ahhoz, hogy ha rákattintunk ne látszon a menü, onClick-el beállítjuk az IsActive-ot false-ra mindenhol */}
+                        <a href="#topics" onClick={() => setIsActive(false)}>
+                            Course Details
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#info" onClick={() => setIsActive(false)}>
+                            About
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#blog" onClick={() => setIsActive(false)}>
+                            Blog
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#testimonials" onClick={() => setIsActive(false)}>
+                            Testimonials
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
