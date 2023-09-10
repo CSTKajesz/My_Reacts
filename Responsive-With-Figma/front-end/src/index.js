348d9b64aa40e10452b055526279046a1561be65
CSTKajesz <tothkajesz@gmail.com>
2023-09-10 16:28:46 +0200

    Change

--- Responsive-With-Figma/front-end/src/index.js
+++
@@ -1,8 +0,0 @@
-import Header from "./header/Header";
-import Topics from "./topics/Topics";
-import Info from "./info/Info";
-import Blog from "./Blog/Blog";
-import Testimonials from "./Testimonials/Testimonials";
-import Footer from "./Footer/Footer"
-
-export { Header, Topics, Info, Blog, Testimonials, Footer }
